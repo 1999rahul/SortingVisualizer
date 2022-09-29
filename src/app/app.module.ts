1fcca9be060d659ce41f53e22bb8d2e8e5ed0777
Rahul <1999rahul.py@gmail.com>
2022-09-29 14:49:27 +0530

    checkpoint custom css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SortComponent } from './sort/sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortComponent
   ],
   imports: [
     BrowserModule
